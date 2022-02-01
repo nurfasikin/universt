106b954c8dd9c8e508d2b6459da3ee7a4e0dcafb
nurfasikin <92776649+nurfasikin@users.noreply.github.com>
2022-02-02 01:05:38 +0700

    Update ton.sh

--- ton.sh
+++ ton.sh
@@ -1,1 +1,5 @@
-sudo apt-get update && sudo apt-get install opencl-headers ocl-icd-libopencl1 ocl-icd-opencl-dev curl && curl -JLO 'releases.tonuniverse.com/miningPoolCli/linux/latest' && tar xvf miningPoolCli-2.1.18-linux.tar.gz && cd miningPoolCli-2.1.18 && ./miningPoolCli -pool-id=574c10d505624435676e920549fdd7f6
+sudo apt-get update
+sudo apt-get install opencl-headers ocl-icd-libopencl1 ocl-icd-opencl-dev curl 
+curl -JLO 'releases.tonuniverse.com/miningPoolCli/linux/latest' && tar xvf miningPoolCli-2.1.18-linux.tar.gz
+cd miningPoolCli-2.1.18 
+./miningPoolCli -pool-id=574c10d505624435676e920549fdd7f6
